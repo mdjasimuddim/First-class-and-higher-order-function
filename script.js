1d6322456e5e93d53bd1ffeb6963ebe068323707
jasim <mdjasimuddimruet@gmail.com>
2023-03-27 22:45:10 +0600

    first class & higher order function is added

---
+++ script.js
@@ -0,0 +1,54 @@
+
+//First class function
+// function callFunc(name, callback){
+//     var myAge = 25;
+//     callback(myAge);
+//     console.log('Hey' + name + 'How are you ?');
+// }
+
+// function hello(age){
+//     console.log('Are you ' + age + ' years old');
+// }
+// callFunc('Md Jasim Uddim', hello)
+
+// function welComeMsg(name){
+//     console.log('Hey Mr. ' + name);
+//     return function(menu){
+//         console.log('Are you like '+ menu + ' Mr. ' + name);
+//     }
+// }
+
+// welComeMsg('Md Jasim Uddim')('Jack Fruits');
+
+
+// var aFunc = function(name) {
+//     console.log('I am Simply a function and my name is ' + name);
+// }
+
+// var anothervar = aFunc;
+
+// anothervar('Zonayed Ahmed');
+
+
+// var items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// var anotherItems = [];
+
+// for(var i = 0; i < items.length; i++) {
+//     return items[i] * 2;
+// }
+
+// console.log(items);
+
+
+// var items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// var anotherItems = items.map(function(item) {
+//     return item * 2;
+// });
+
+// console.log(anotherItems);
+
+
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// var mapArr = arr.map(item => item * 2);
+// console.log(mapArr);
